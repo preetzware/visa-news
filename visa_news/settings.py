a3a4025f1348d73df9e8f0a9e81ba153ca691939
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-01 16:54:24 +0000

    Configure SECRET_KEY as environment variable, update settings and redeploy app on heroku

--- visa_news/settings.py
+++ visa_news/settings.py
@@ -19,6 +19,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
