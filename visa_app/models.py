718b44ad0e596438aee8ea59b819d2950dddfe08
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-02 22:31:12 +0000

    Modify and update article model

--- visa_app/models.py
+++ visa_app/models.py
@@ -3,12 +3,32 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-class Post(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="visa_article"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="visa_articles")
     content = models.TextField()
+    published_at = models.DateTimeField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True, related_name='visa_articles')
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    likes = models.ManyToManyField(User, related_name="article_likes", blank=True)
+
+    class Meta:
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('article_detail', kwargs={'slug': self.slug})
+
+    class Category(models.Model):
+        name = models.CharField(max_length=100)
+        slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
