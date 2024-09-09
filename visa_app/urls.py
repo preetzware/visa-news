43217f9a37f33414af3dcf1df5ab8d834d1348a0
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-09 20:38:44 +0000

    Add article_detail view

--- visa_app/urls.py
+++ visa_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArticleList.as_view(), name='home'),
+    path('', views.ArticleList.as_view(), name='home'), # Home page URL
+    path('<slug:slug>/', views.article_detail, name='article_detail'), # URL to opening article 
     path('search/', views.search_view, name='search'),  # New URL pattern for search
 ]
--- visa_app/views.py
+++ visa_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Article
 
@@ -8,6 +8,11 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.all()
     template_name = "visa_app/index.html"
     paginate_by = 6
+
+def article_detail(request):
+    queryset = Article.objects.filter(status=1)
+    article = get_object_or_404(queryset, slug=slug)
+    return render(request, "visa_app/article_detail.html", {"article": article},)    
 
 def search_view(request):
     query = request.GET.get('query')
