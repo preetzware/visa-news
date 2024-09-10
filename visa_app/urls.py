6246318434726493747db54e4bdcc8ad10ac336a
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-10 19:46:38 +0000

    Debug and Fix URL conflict issue. Update urlpattern in visa_app/urls.py

--- visa_app/urls.py
+++ visa_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'), # Home page URL
-    path('<slug:slug>/', views.article_detail, name='article_detail'), # URL to opening article 
+    path('articles/<slug:slug>/', views.article_detail, name='article_detail'), # URL to opening article 
     path('search/', views.search_view, name='search'),  # New URL pattern for search
 ]
