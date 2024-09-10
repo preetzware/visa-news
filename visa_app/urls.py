fafbb35a9ef375aa82707db67a276ccdb2e85c7b
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-10 22:27:27 +0000

    Modify index.html to include both the carousel and a list of six articles.

--- visa_app/urls.py
+++ visa_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index_view, name='home'),  # Index page URL
     path('', views.ArticleList.as_view(), name='home'), # Home page URL
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'), # URL to opening article 
     path('search/', views.search_view, name='search'),  # New URL pattern for search
--- visa_app/views.py
+++ visa_app/views.py
@@ -12,7 +12,14 @@ class ArticleList(generic.ListView):
 def article_detail(request, slug):
     queryset = Article.objects.filter(status=1)
     article = get_object_or_404(queryset, slug=slug)
-    return render(request, "visa_app/article_detail.html", {"article": article},)    
+    return render(request, "visa_app/article_detail.html", {"article": article},)  
+
+def index_view(request):
+    latest_articles = Article.objects.all().order_by('-published_at')[:3]  # Latest 3 articles for the carousel
+    other_articles = Article.objects.all().order_by('-published_at')[3:9]  # The rest of the articles for cards
+    return render(request, 'visa_app/index.html', {
+        'latest_articles': latest_articles,
+        'other_articles': other_articles,}) 
 
 def search_view(request):
     query = request.GET.get('query')
