86588b0c4e280b649628c48f3d50b0d78a5025c6
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-10 16:04:39 +0000

    Register visanews models in admin.py

--- visanews/admin.py
+++ visanews/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import VisanewsArticle, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(VisanewsArticle)
+class VisanewsArticleAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
