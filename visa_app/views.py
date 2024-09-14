4096d73ebbaa6710717921d0235af8feb99d9199
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-14 21:41:35 +0000

    Create and post comment to the database as an authenticated user

--- visa_app/views.py
+++ visa_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from django.views import generic
+from django.contrib import messages
 from .models import Article
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -15,10 +17,26 @@ def article_detail(request, slug):
     article = get_object_or_404(queryset, slug=slug)
     comments = article.comments.all().order_by("-created_on")
     comment_count = article.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.article = article
+            comment.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
+    
+    comment_form = CommentForm()
+
     return render(request, "visa_app/article_detail.html", {
         "article": article,
         "comments": comments,
         "comment_count": comment_count,
+        "comment_form": comment_form,
     })  
 
 def index_view(request):
