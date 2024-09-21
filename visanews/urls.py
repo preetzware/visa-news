082f7fcbb3782f2f8e99f7e2d0a240fab55ccd95
Devianee Ramdin <devianeeramdin@outlook.com>
2024-09-21 23:27:03 +0000

    Add views and urls  for visanews app

--- visanews/urls.py
+++ visanews/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.VisanewsList.as_view(), name='visanews_list'),
     path('<slug:slug>/', views.visanews_detail, name='visanews_detail'),
+    path('visanews/search/', views.visanews_search_view, name='visanews_search'),  # New URL pattern for search in visanews
+    path('visanews/like_article/<int:article_id>/', views.like_article, name='visanews_like_article'),  # Like article URL for visanews
+    path('visanews/dislike_article/<int:article_id>/', views.dislike_article, name='visanews_dislike_article'),
+    path('visanews/articles/<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='visanews_comment_edit'),  # Edit comment in visanews
+    path('visanews/articles/<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='visanews_comment_delete'),  # Delete comment in visanews
 ]
--- visanews/views.py
+++ visanews/views.py
@@ -1,18 +1,136 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.db.models import Q
 from django.views import generic
-from .models import VisanewsArticle
+from django.http import HttpResponseRedirect, JsonResponse
+from django.contrib import messages
+from .models import VisanewsArticle, Comment
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
+import json
 
-# Create your views here.
+VisanewsArticle.objects.filter(status=1).values('category__name').distinct()
 
+# Visanews Article List View
 class VisanewsList(generic.ListView):
-    queryset = VisanewsArticle.objects.filter(status=1, category__name__icontains="Visa News") # Selects articles from Visa News category
+    queryset = VisanewsArticle.objects.filter(status=1, category__name__icontains="Visa News")
     model = VisanewsArticle
-    template_name = 'visanews/visanews_list.html'  # Template
-    context_object_name = 'articles'  # Name of the context variable to use in the template
-    paginate_by = 6 
+    template_name = 'visanews/visanews_list.html'
+    paginate_by = 6
 
+# Visanews Article Detail View
 def visanews_detail(request, slug):
-    article = get_object_or_404(VisanewsArticle, slug=slug, status=1)
-    return render(request, 'visanews/visanews_detail.html', {'article': article}) 
+    queryset = VisanewsArticle.objects.filter(status=1)
+    article = get_object_or_404(queryset, slug=slug)
+    like_count = article.number_of_likes()
+    dislike_count = article.number_of_dislikes()
+    comments = article.comments.all().order_by("-created_on")
+    comment_count = article.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.article = article
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thank you! Your comment is awaiting approval.'
+            )
+
+    comment_form = CommentForm()
+
+    return render(request, "visanews/visanews_detail.html", {
+        "article": article,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form,
+        "like_count": like_count,
+        "dislike_count": dislike_count,
+    })
+
+# Search View for Visanews
+def visanews_search_view(request):
+    query = request.GET.get('query')
+    if query:
+        search_results = VisanewsArticle.objects.filter(
+            Q(title__icontains=query) |
+            Q(excerpt__icontains=query) |
+            Q(category__name__icontains=query)
+        ).distinct()
+    else:
+        search_results = VisanewsArticle.objects.none()
+    return render(request, 'visanews/visanews_search_results.html', {'results': search_results, 'query': query})
 
 
+# Like and Dislike Article View for Visanews
+@require_POST
+def like_article(request, article_id):
+    article = get_object_or_404(VisanewsArticle, id=article_id)
+    action = json.loads(request.body).get('action')
+
+    if action == 'like':
+        article.likes.add(request.user)
+        article.dislikes.remove(request.user)
+    elif action == 'dislike':
+        article.dislikes.add(request.user)
+        article.likes.remove(request.user)
+
+    return JsonResponse({
+        'success': True,
+        'like_count': article.likes.count(),
+        'dislike_count': article.dislikes.count(),
+    })
+
+# Dislike Article View for Visanews
+@require_POST
+def dislike_article(request, article_id):
+    article = get_object_or_404(VisanewsArticle, id=article_id)
+    action = json.loads(request.body).get('action')
+
+    if action == 'dislike':
+        article.dislikes.add(request.user)
+        article.likes.remove(request.user)
+    elif action == 'like':
+        article.likes.add(request.user)
+        article.dislikes.remove(request.user)
+
+    return JsonResponse({
+        'success': True,
+        'like_count': article.likes.count(),
+        'dislike_count': article.dislikes.count(),
+    })
+
+# Edit Comment View for Visanews
+def comment_edit(request, slug, comment_id):
+    queryset = VisanewsArticle.objects.filter(status=1)
+    article = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        if comment_form.is_valid() and comment.user == request.user:
+            comment = comment_form.save(commit=False)
+            comment.article = article
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment updated and awaiting approval!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('visanews_detail', args=[slug]))
+
+
+# Delete Comment View for Visanews
+def comment_delete(request, slug, comment_id):
+    queryset = VisanewsArticle.objects.filter(status=1)
+    article = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.user == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('visanews_detail', args=[slug]))
